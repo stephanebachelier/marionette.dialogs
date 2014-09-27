65c3c06870f78491e95163d486306c7fc683150f
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-09-27 07:19:10 +0200

    feat(examples): add alert.error example

---
+++ examples/override/scripts/views/dialogs/alert.error.js
@@ -0,0 +1,14 @@
+define([
+  'marionette.dialogs'
+], function (dialogs) {
+  'use strict';
+
+  return dialogs.alert.extend({
+    // basically this view does nothing more than adding a class `alert--error`
+    // and to avoid removing anything predefined className it append the className
+    // to parent (read prototype) property `className`
+    className: function () {
+      return dialogs.alert.prototype.className + ' ' + 'alert--error';
+    }
+  });
+});
--- examples/override/scripts/views/dialogs/index.js
+++ examples/override/scripts/views/dialogs/index.js
@@ -1,12 +1,14 @@
 define([
-  './alert.success'
-], function (AlertSuccess) {
+  './alert.success',
+  './alert.error'
+], function (AlertSuccess, AlertError) {
   'use strict';
 
   var customDialogs = Object.create(null); // yes we want a hash like
 
   // it's only for example. The registry might need some logic :)
   customDialogs['alert.success'] = AlertSuccess;
+  customDialogs['alert.error'] = AlertError;
 
   return customDialogs;
 });
