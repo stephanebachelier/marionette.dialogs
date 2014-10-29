42fe23d7582016e585ffab52370dba893a438a1c
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-10-29 22:32:01 +0100

    feat(example): switch to dialog factory

--- examples/basic/scripts/controllers/dialogs.js
+++ examples/basic/scripts/controllers/dialogs.js
@@ -1,13 +1,18 @@
 define([
   'marionette',
-  './dialog.builder'
+  'marionette.dialogs',
+  'json!config/dialogs.json'
 ],
-function (Marionette, dialogBuilder) {
+function (Marionette, dialogs, config) {
   'use strict';
 
   return Marionette.Controller.extend({
     initialize: function (options) {
       this.region = this.options.region;
+
+      this.factory = new dialogs.Factory(dialogs, {
+        config: config
+      });
     },
 
     showDialog: function (type) {
@@ -15,7 +20,20 @@ function (Marionette, dialogBuilder) {
         return;
       }
 
-      dialogBuilder.showDialog(type, this.region);
+      var self = this;
+
+      require(['hbs!templates/' + type], function (template) {
+
+        self.factory.render(self.region, type, {template: template});
+
+        self.region.$el.toggleClass('active');
+        // listen for view destruction
+        self.region.currentView.on('destroy', function (view) {
+          self.region.$el.toggleClass('active');
+        }, self);
+      });
+
+
     }
   });
 });
